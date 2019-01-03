6f5a86e85e238a22792fc29e33837aca195fb258
LuanFelipeRSanches <unopararchspire@hotmail.com>
2019-01-03 17:30:17 -0300

    Iniciando Projeto Java Web

---
+++ Vendas/src/br/com/vendas/main/GerarTabela.java
@@ -0,0 +1,11 @@
+package br.com.vendas.main;
+
+import br.com.vendas.util.HibernateUtil;
+
+public class GerarTabela {
+	public static void main(String[] args) {
+		HibernateUtil.getSessionFactory();
+		HibernateUtil.getSessionFactory().close();
+
+	}
+}
